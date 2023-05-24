0a38d7abb7afd208986a159126baafa8d469a6ac
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-05-24 20:52:55 +0600

    code refactoring

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function func() {
-    var num0 = Number(document.getElementById("num0").value);
-    var num1 = Number(document.getElementById("num1").value);
-    var num2 = Number(document.getElementById("num2").value);
-    var num3 = Number(document.getElementById("num3").value);
-    var num4 = Number(document.getElementById("num4").value);
-    var num5 = Number(document.getElementById("num5").value);
-    var result = num0 + num1 + num2 - num3 - num4 + num5;
-    console.log(num1 + num2 - num3 - num4 + num5);
+    let num0 = Number(document.getElementById("num0").value);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let num3 = Number(document.getElementById("num3").value);
+    let num4 = Number(document.getElementById("num4").value);
+    let num5 = Number(document.getElementById("num5").value);
+    let result = num0 + num1 * 2000 + num2 * 5000 - num3 - num4 + num5 + "тг";
+    if (num1 > 15) {
+        result = num0 + num1 * 2000 + num2 * 6000 - num3 - num4 + num5 + "тг";
+    }
+
+
+
+
     document.getElementById("result").innerHTML = result;
 }
